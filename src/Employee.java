6788579a3daf7287a4e091bf4921ebc3817ffafb
Westender <javaxwestender@gmail.com>
2023-07-12 16:45:12 +0300

    Add: constructor, getters and setters

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,42 @@
 public class Employee {
+
+    private String employee;
+    private int department;
+    private int salary;
+    static int id;
+    static int counter;
+
+    public Employee (String employee, int department, int salary, int id){
+        this.setEmployee(employee);
+        this.setDepartment(department);
+        this.setSalary(salary);
+        this.id = id;
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee = employee;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
     public static void main(String[] args) {
     }
 }
